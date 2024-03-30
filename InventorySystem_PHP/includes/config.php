529aa688e88ee147c4d42488eb9e79188fa93e67
dabi2 <dabiangjalong3832gmail.com>
2024-03-30 12:35:10 +0530

    first commit

---
+++ InventorySystem_PHP/includes/config.php
@@ -0,0 +1,19 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+
+|--------------------------------------------------------------------------
+| Author: ICFAI
+| Project Name: Inventory Management
+| Version: v2
+|
+|
+|
+|
+*/
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_NAME', 'inventory_system' );        // Set database name
+
+?>
